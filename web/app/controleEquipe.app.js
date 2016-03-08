d220cdb5ce1c06a9960d437475c802ed0545ff91
dipaschoal <di.paschoalini@gmail.com>
2016-03-07 22:48:11 -0300

    Web

--- web/app/controleEquipe.app.js
+++ web/app/controleEquipe.app.js
@@ -1,47 +1,35 @@
-(function () {
-    'use strict';
-    angular.module('ControleEquipeApp', ['ngRoute']);
+  var ControleEquipeApp = angular.module('ControleEquipeApp', ['ngRoute', 'MainController', 'CadastroAlocacaoController', 'CadastroLancamentoController', 'CadastroProjetoController', 'CadastroRecursoController']);
 
-    ControleEquipeApp.$inject = ['CadastroAlocacaoController', 'CadastroLancamentoController', 'CadastroProjetoController', 'CadastroRecursoController'];
+  // configure our routes
+  ControleEquipeApp.config(['$routeProvider',
+  function ($routeProvider) {
 
+          $routeProvider
+          // route for the home page
+              .when('/', {
+              templateUrl: 'index.html',
+              controller: 'MainController'
+          })
 
-    function ControleEquipeApp($routeProvider, CadastroAlocacaoController, CadastroLancamentoController, CadastroProjetoController, CadastroRecursoController) {
+          // route for the about page
+          .when('/alocacao', {
+              templateUrl: 'partials/alocacao.html',
+              controller: 'CadastroAlocacaoController'
+          })
 
-        var vm = this;
+          .when('/lancamento', {
+              templateUrl: 'partials/lancamento.html',
+              controller: 'CadastroLancamentoController'
+          })
 
-        // configure our routes
-        vm.getRoute = getRoute
-        vm.getRoute($routeProvider);
+          .when('/manter/projeto', {
+              templateUrl: 'partials/manter-projeto.html',
+              controller: 'CadastroProjetoController'
+          })
 
-        function getRoute(routeProvider) {
+          .when('/manter/recurso', {
+              templateUrl: 'partials/manter-recurso.html',
+              controller: 'CadastroRecursoController'
+          });
 
-            routeProvider
-            // route for the home page
-                .when('/', {
-                templateUrl: 'index.html',
-                controller: 'MainController'
-            })
-
-            // route for the about page
-            .when('/alocacao', {
-                templateUrl: 'partials/alocacao.html',
-                controller: 'CadastroAlocacaoController'
-            })
-
-            .when('/lancamento', {
-                templateUrl: 'partials/lancamento.html',
-                controller: 'CadastroLancamentoController'
-            })
-
-            .when('/manter/projeto', {
-                templateUrl: 'partials/manter-projeto.html',
-                controller: 'CadastroProjetoController'
-            })
-
-            .when('/manter/recurso', {
-                templateUrl: 'partials/manter-recurso.html',
-                controller: 'CadastroRecursoController'
-            });
-        }
-    }
-}());
+  }]);
